9eac9836ee51718e402ba579313309bc38604258
Tùng Phạm <tungpham010203@gmail.com>
2023-02-10 00:20:14 +0700

    commit message

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+
+const int SCREEN_WIDTH = 540, SCREEN_HEIGHT = 960;
+
+const std::string WINDOW_TITLE = "An Implementation of Code.org Painter";
+
+//void quitSDL(SDL_Window* window, SDL_Renderer* renderer);
+
+
+int main(int argc, char* argv[])
+{
+ SDL_Window* window;
+ SDL_Renderer* renderer;
+
+ // Your drawing code here
+ // use SDL_RenderPresent(renderer) to show it
+
+
+ return 0;
+}
