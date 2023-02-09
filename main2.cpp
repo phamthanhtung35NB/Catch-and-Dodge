e6274954f0d4bebb6d1bcdf33b800d1278401475
Tùng Phạm <tungpham010203@gmail.com>
2023-02-10 00:27:03 +0700

    lan2

---
+++ main2.cpp
@@ -0,0 +1,21 @@
+Các lệnh cơ bản trong Git bao gồm:
+
+git init: Khởi tạo một repository mới.
+
+git clone: Sao chép một repository từ máy chủ về máy cục bộ của bạn.
+
+git add: Thêm một tập tin hoặc thư mục vào stage.
+
+git commit: Ghi nhận tất cả các thay đổi đã được stage vào lịch sử repository.
+
+git push: git push --set-upstream origin main //Đẩy tất cả các commit mới lên máy chủ. ok
+
+git pull: Tải về tất cả các thay đổi từ máy chủ và merge với bản làm việc của bạn. ddoongf booj tuwf treen mangj veef mays
+
+git diff: Hiển thị tất cả các thay đổi trong tập tin so với phiên bản trước đó.
+
+git log: Hiển thị lịch sử commit trong repository.
+
+git branch: Quản lý các nhánh trong repository.
+
+git checkout: Chuyển đến một nhánh hoặc phiên bản đã có sẵn.
