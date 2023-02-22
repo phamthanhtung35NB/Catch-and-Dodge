7f8bab75bceb90dd229f72cfd5906e7b4625b0e6
Tùng Phạm Thanh <tungpham010203@gmail.com>
2023-02-23 00:36:29 +0700

    ban upgare co doc va ghi file khi tao tk mơi

--- docGhiFile.cpp
+++ docGhiFile.cpp
@@ -5,9 +5,13 @@
 #include "erom.h"
 // khiểmtra tk mk trong data
 std::string check(std::string& name_login,std::string& pass_login);
-std::string checkTrongFile(std::string& name_login,std::string& pass_login){
+void ghiFile();
 
+
+std::string line, name_, pass_,diem_;
+std::string checkTrongFile(std::string& name_login,std::string& pass_login){
     std::ifstream file_data("data/dataLuuTam.txt");
+   
 
     if (!file_data.is_open()) {
         std::cout << "Failed to open file" << std::endl;
@@ -42,4 +46,10 @@ std::string checkTrongFile(std::string& name_login,std::string& pass_login){
         
     }
     file_data.close();
+}
+void ghiFile(std::string& name_login,std::string& pass_login,std::string& diem){
+    std::ofstream outfile("data/dataLuuTam.txt", std::ios::app);
+    outfile << name_login << " " << pass_login << " " << diem << std::endl;
+    // đóng file
+    outfile.close();
 }
