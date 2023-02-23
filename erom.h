3cb8c465f77ba43435d3be2d5b7196c79dca3e1a
Tùng Phạm Thanh <tungpham010203@gmail.com>
2023-02-24 00:07:44 +0700

    them chuc năng sua diem khi choi xong

--- erom.h
+++ erom.h
@@ -11,3 +11,56 @@ extern std::string arr[4];
 // arr[3]=diem
 
 #endif
+
+/*
+PS C:\Users\admin\Desktop\PhamThanhTung_22021128> git push 
+Enumerating objects: 9, done.
+Counting objects: 100% (9/9), done.
+Delta compression using up to 12 threads
+Compressing objects: 100% (5/5), done.
+Writing objects: 100% (5/5), 811 bytes | 811.00 KiB/s, done.
+Total 5 (delta 4), reused 0 (delta 0), pack-reused 0
+remote: Resolving deltas: 100% (4/4), completed with 4 local objects.
+To https://github.com/phamthanhtung35NB/du_an_big.git
+   9a3b76d..7f8bab7  main -> main
+PS C:\Users\admin\Desktop\PhamThanhTung_22021128> 
+ *  History restored 
+
+ 4 files changed, 80 insertions(+), 51 deletions(-)
+PS C:\Users\admin\Desktop\PhamThanhTung_22021128> git push
+To https://github.com/phamthanhtung35NB/du_an_big.git
+ ! [rejected]        main -> main (fetch first)
+error: failed to push some refs to 'https://github.com/phamthanhtung35NB/du_an_big.git'
+hint: Updates were rejected because the remote contains work that you do
+hint: not have locally. This is usually caused by another repository pushing
+hint: to the same ref. You may want to first integrate the remote changes
+hint: See the 'Note about fast-forwards' in 'git push --help' for details.
+PS C:\Users\admin\Desktop\PhamThanhTung_22021128> git push
+To https://github.com/phamthanhtung35NB/du_an_big.git
+ ! [rejected]        main -> main (fetch first)
+error: failed to push some refs to 'https://github.com/phamthanhtung35NB/du_an_big.git'
+hint: Updates were rejected because the remote contains work that you do
+hint: not have locally. This is usually caused by another repository pushing
+hint: to the same ref. You may want to first integrate the remote changes
+hint: (e.g., 'git pull ...') before pushing again.
+hint: See the 'Note about fast-forwards' in 'git push --help' for details.
+PS C:\Users\admin\Desktop\PhamThanhTung_22021128> git pull
+remote: Enumerating objects: 4, done.
+remote: Counting objects: 100% (4/4), done.
+remote: Compressing objects: 100% (2/2), done.
+remote: Total 3 (delta 1), reused 0 (delta 0), pack-reused 0
+Unpacking objects: 100% (3/3), 696 bytes | 18.00 KiB/s, done.
+From https://github.com/phamthanhtung35NB/du_an_big
+   7f8bab7..90e85cb  main       -> origin/main
+There is no tracking information for the current branch.
+Please specify which branch you want to merge with.
+See git-pull(1) for details.
+
+    git pull <remote> <branch>
+
+If you wish to set tracking information for this branch you can do so with:
+
+    git branch --set-upstream-to=origin/<branch> main
+
+PS C:\Users\admin\Desktop\PhamThanhTung_22021128>
+*/
